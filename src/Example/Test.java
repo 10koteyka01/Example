b331cad971f7cef91d309114522a9f6bb6b79135
koteyka <tihonenkovk@mail.ru>
2022-03-14 17:15:58 +0700

    Changes

---
+++ src/Example/Test.java
@@ -0,0 +1,11 @@
+package Example;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+
+    static {
+        System.out.println("test");
+    }
+}
